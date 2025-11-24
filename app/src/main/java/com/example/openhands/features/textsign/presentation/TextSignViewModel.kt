9b39012d07e4b79b47e186c13bfec1043b7d3423
Isaac71412079 <isaac.rivero@ucb.edu.bo>
2025-11-23 21:31:25 -0400

    primera frase implementada

--- app/src/main/java/com/example/openhands/features/textsign/presentation/TextSignViewModel.kt
+++ app/src/main/java/com/example/openhands/features/textsign/presentation/TextSignViewModel.kt
@@ -43,58 +43,66 @@ class TextSignViewModel(
 
         val newVideoQueue = mutableListOf<Int>()
 
-        val exceptions = mapOf(
-            "hola" to R.raw.hola,
-            "mundo" to R.raw.mundo
-
+        val phraseExceptions = mapOf(
+            "buenos dias" to R.raw.buenosdias, // Asegúrate de tener buenos_dias.mp4
         )
 
-        val words = textToTranslate.split(' ').filter { it.isNotBlank() }
+        val exceptions = mapOf(
+            "hola" to R.raw.hola,
+            "mundo" to R.raw.mundo,
+            "buenos" to R.raw.buenos,
+        )
 
-        for (word in words) {
-            if (exceptions.containsKey(word)) {
-                // La palabra es una excepción, añadimos solo su video específico
-                newVideoQueue.add(exceptions[word]!!)
-            } else {
-                // La palabra no es una excepción, la deletreamos
-                for (char in word) {
-                    val videoResId = when (char.toString()) {
-                        "a" -> R.raw.letra_a
-                        "b" -> R.raw.letra_b
-                        "c" -> R.raw.letra_c
-                        "d" -> R.raw.letra_d
-                        "e" -> R.raw.letra_e
-                        "f" -> R.raw.letra_f
-                        "g" -> R.raw.letra_g
-                        "h" -> R.raw.letra_h
-                        "i" -> R.raw.letra_i
-                        "j" -> R.raw.letra_j
-                        "k" -> R.raw.letra_k
-                        "l" -> R.raw.letra_l
-                        "ll" -> R.raw.letra_ll
-                        "m" -> R.raw.letra_m
-                        "n" -> R.raw.letra_n
-                        "ñ" -> R.raw.letra_ne
-                        "o" -> R.raw.letra_o
-                        "p" -> R.raw.letra_p
-                        "q" -> R.raw.letra_q
-                        "r" -> R.raw.letra_r
-                        "rr" -> R.raw.letra_rr
-                        "s" -> R.raw.letra_s
-                        "t" -> R.raw.letra_t
-                        "u" -> R.raw.letra_u
-                        "v" -> R.raw.letra_v
-                        "w" -> R.raw.letra_w
-                        "x" -> R.raw.letra_x
-                        "y" -> R.raw.letra_y
-                        "z" -> R.raw.letra_z
-                        else -> null
+        if (phraseExceptions.containsKey(textToTranslate)) {
+            // Si es "buenos dias", añade solo ese video y termina.
+            newVideoQueue.add(phraseExceptions[textToTranslate]!!)
+        } else {
+            val words = textToTranslate.split(' ').filter { it.isNotBlank() }
+
+            for (word in words) {
+                if (exceptions.containsKey(word)) {
+                    // La palabra es una excepción, añadimos solo su video específico
+                    newVideoQueue.add(exceptions[word]!!)
+                } else {
+                    // La palabra no es una excepción, la deletreamos
+                    for (char in word) {
+                        val videoResId = when (char.toString()) {
+                            "a" -> R.raw.letra_a
+                            "b" -> R.raw.letra_b
+                            "c" -> R.raw.letra_c
+                            "d" -> R.raw.letra_d
+                            "e" -> R.raw.letra_e
+                            "f" -> R.raw.letra_f
+                            "g" -> R.raw.letra_g
+                            "h" -> R.raw.letra_h
+                            "i" -> R.raw.letra_i
+                            "j" -> R.raw.letra_j
+                            "k" -> R.raw.letra_k
+                            "l" -> R.raw.letra_l
+                            "ll" -> R.raw.letra_ll
+                            "m" -> R.raw.letra_m
+                            "n" -> R.raw.letra_n
+                            "ñ" -> R.raw.letra_ne
+                            "o" -> R.raw.letra_o
+                            "p" -> R.raw.letra_p
+                            "q" -> R.raw.letra_q
+                            "r" -> R.raw.letra_r
+                            "rr" -> R.raw.letra_rr
+                            "s" -> R.raw.letra_s
+                            "t" -> R.raw.letra_t
+                            "u" -> R.raw.letra_u
+                            "v" -> R.raw.letra_v
+                            "w" -> R.raw.letra_w
+                            "x" -> R.raw.letra_x
+                            "y" -> R.raw.letra_y
+                            "z" -> R.raw.letra_z
+                            else -> null
+                        }
+                        videoResId?.let { newVideoQueue.add(it) }
                     }
-                    videoResId?.let { newVideoQueue.add(it) }
                 }
             }
         }
-
         videoQueue = newVideoQueue
         viewModelScope.launch {
             homeUseCase.saveTranslation(textToTranslate)
